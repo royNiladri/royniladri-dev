e33ba65a297c016f1d96fa425efb64991b83470c
royNiladri <callniladri.pro@gmail.com>
2020-11-21 16:48:49 +0530

    removed invalid role

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="footer">
-            <span role="copyright">&copy; {new Date().getFullYear()} Niladri Roy</span>
+            <span>&copy; {new Date().getFullYear()} Niladri Roy</span>
         </footer>
     );
 }
