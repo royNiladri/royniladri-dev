30ce6bee5e0784cf62c36ab0880ed44452ecaef2
Niladri <royNiladri@users.noreply.github.com>
2021-04-10 22:24:22 +0530

    Remove unused variabled

--- src/shared/Footer/Support.tsx
+++ src/shared/Footer/Support.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import './Footer.scss';
 
 import bmc from '../../assets/images/bmc-logo.svg';
-import patreon from '../../assets/images/Patreon_logomark.svg';
 
 function Support() {
     return (
         <div className="support">
             <div className="support-links">
-                {/* <span><a href='https://www.patreon.com/royniladri'>
-                    <img src={patreon} height="32" alt="Patreon Logo"/>
-                </a> Become a Patreon</span>      */}
                 <span><a href='https://buymeacoffee.com/royniladri'>
-                    <img src={bmc} height="32" alt="Buy me a Coffee Logo"/> Buy me a Coffee</a></span>
+                    <img src={bmc} height="32" alt="Buy me a Coffee Logo"/> Buy me a Coffee</a>
+                </span>
             </div>
         </div>
     )
